465715b29ef61f3b29f0cd08ca5528f3a21bc7cf
Michael Marner <michael.marner@unisa.edu.au>
2012-07-03 13:55:53 +0930

    Actually save metadata.

--- publications.php
+++ publications.php
@@ -152,4 +152,35 @@ function wcl_publication_add_boxes() {
 	add_meta_box($pubBox['id'], $pubBox['title'], 'show_pub_box', 'wcl_publication');
 }
 
+add_action('save_post', 'wcl_pub_save_data');
+// Save data from meta box
+function wcl_pub_save_data($post_id) {
+	global $meta_box;
+	// verify nonce
+	if (!wp_verify_nonce($_POST['mytheme_meta_box_nonce'], basename(__FILE__))) {
+		return $post_id;
+	}
+	// check autosave
+	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+		return $post_id;
+	}
+	// check permissions
+	if ('page' == $_POST['post_type']) {
+		if (!current_user_can('edit_page', $post_id)) {
+			return $post_id;
+		}
+	} elseif (!current_user_can('edit_post', $post_id)) {
+		return $post_id;
+	}
+	foreach ($meta_box['fields'] as $field) {
+		$old = get_post_meta($post_id, $field['id'], true);
+		$new = $_POST[$field['id']];
+		if ($new && $new != $old) {
+			update_post_meta($post_id, $field['id'], $new);
+		} elseif ('' == $new && $old) {
+			delete_post_meta($post_id, $field['id'], $old);
+		}
+	}
+}
+
 ?>
